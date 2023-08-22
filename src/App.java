b45447ed6c327aec0467b97780b7de4994dfbf7e
Sebastian <sebastiandiaz1222@gmail.com>
2023-08-22 16:00:21 -0700

    a

--- src/App.java
+++ src/App.java
@@ -3,5 +3,6 @@ package src;
 public class App {
     public static void main (String[] args) {
         GUI gui = new GUI();
+        gui.start();
     }
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,10 +1,12 @@
 package src;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class GUI {
 
     private JFrame frame;
+    ImageIcon image = new ImageIcon("icon.png");
 
     public GUI() {
         frame = new JFrame();
@@ -13,5 +15,9 @@ public class GUI {
     public void start() {
         frame.setTitle("Blackjack");
         frame.setSize(400, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setIconImage(image.getImage());
+        frame.setVisible(true);
     }
 }
