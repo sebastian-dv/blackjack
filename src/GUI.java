986fd1be9e0b8b47e003e7e78039b90faca8963c
Sebastian <sebastiandiaz1222@gmail.com>
2023-08-22 20:46:11 -0700

    new changes

--- src/GUI.java
+++ src/GUI.java
@@ -1,23 +1,51 @@
 package src;
 
-import javax.swing.ImageIcon;
+import java.awt.GridBagLayout;
+import java.awt.LayoutManager;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class GUI {
 
     private JFrame frame;
-    ImageIcon image = new ImageIcon("icon.png");
+    private JPanel panel;
+    private JPanel bottomPanel;
+    private JButton play;
+    private JButton hit;
+    private JButton stand;
 
     public GUI() {
         frame = new JFrame();
-    }
-
-    public void start() {
         frame.setTitle("Blackjack");
         frame.setSize(400, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
-        frame.setIconImage(image.getImage());
+
+        panel = new JPanel();
+        panel.setSize(400, 300);
+
+        bottomPanel = new JPanel();
+
+    }
+
+    public void start() {
+        GridBagLayout grid = new GridBagLayout();
+        frame.setLayout(grid);
+
+        play = new JButton("Play");
+        play.setSize(100, 70);
+        bottomPanel.add(play);
+        
+        frame.add(panel);
+        frame.add(bottomPanel);
+        frame.setVisible(true);
+    }
+
+    public void play() {
+
         frame.setVisible(true);
     }
 }
